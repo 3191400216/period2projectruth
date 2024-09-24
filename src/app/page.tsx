4795a6059c03e6222713b9e4e9b779ace252fabd
Ruth <[3191400216@k12.hi.us]>
2024-09-24 08:50:07 -1000

    added db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db"
 
 const mockUrls = [
   "https://utfs.io/f/9b247e72-b2ac-4f93-8800-13e75784e016-1ody.jpg",
@@ -14,10 +15,13 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
       <main className="">
         <div className="flex flex-wrap gap-4">
+          {posts.map((post)=> (
+          <div key={post.id}>{post.name}</div>))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
               <img src={image.url} />
