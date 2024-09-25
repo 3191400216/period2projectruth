69f869ec5d3f74df32497c975709315405b0535e
Ruth <[3191400216@k12.hi.us]>
2024-09-25 08:57:13 -1000

    labeling pictures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { db } from "~/server/db"
 
-const mockUrls = [
+/*const mockUrls = [
   "https://utfs.io/f/9b247e72-b2ac-4f93-8800-13e75784e016-1ody.jpg",
   "https://utfs.io/f/3538f835-0ea1-4f92-8f97-3db33294f447-1ody.jpg",
   "https://utfs.io/f/35522c16-42ed-438e-9dd1-f081243e5bde-1ody.jpg",
@@ -14,17 +14,18 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
+*/
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany();
   return (
       <main className="">
         <div className="flex flex-wrap gap-4">
-          {posts.map((post)=> (
-          <div key={post.id}>{post.name}</div>))}
-        {mockImages.map((image) => (
+        
+        {[...images].map((image) => (
           <div key={image.id} className="w-48">
               <img src={image.url} />
+              <div>{image.name}</div> 
         </div>
         ))}
         </div>
